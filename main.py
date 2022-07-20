606f185ccec7f6bf4eae7d2b10067d614083a8b6
Vineshvinu <92975459+Vineshvinu@users.noreply.github.com>
2022-07-20 22:42:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 API_ID = "15806487"
 API_HASH = "c7c7fbd61954591c16599a8330faa2e3"
@@ -13,15 +14,22 @@ VINESH = Client(
 )
 
 
+@VINESH.on_message(filters.command("about"))
+async def about_cmd(client, message):
+    await message.reply_text("Bot status")
+
+START_BUTTONS = [[
+  InlineKeyboardButton("JOIN HERE", url="t.me/tamil_hackers_moviess")
+]]
+
 @VINESH.on_message(filters.command("start"))
 async def start_cmd(client, message):
     await message.reply_photo(
         photo="https://telegra.ph/file/c747f9fd6721860623ee1.jpg",
         caption="Hello {}  I am Rolex")
+        reply_markup=InlineKeyboardMarkup(START_BUTTONS)
+    )
 
-@VINESH.on_message(filters.command("about"))
-async def about_cmd(client, message):
-    await message.reply_text("Bot status")
 
 print("Bot started")
 
