70a9d66f111bab2d4fd151d28f7579518eb973ff
Vineshvinu <92975459+Vineshvinu@users.noreply.github.com>
2022-07-20 21:07:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 from pyrogram import Client, filters
+
+API_ID = ""
+API_HASH = ""
+BOT_TOKEN = ""
+
+
+VINESH = Client(
+    name="VineshNew",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+print("Bot started")
+
+VINESH.run()
