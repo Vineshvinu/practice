248c88b9ef4b1dc5e6fda4d1a3b013bf7eef5ef3
Vineshvinu <92975459+Vineshvinu@users.noreply.github.com>
2022-07-21 16:50:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,11 +20,32 @@ async def about_cmd(client, message):
 
 @VINESH.on_message(filters.command("start"))
 async def start_cmd(client, message):
-    await message.reply_text("*Fuck of*")
+    await message.reply_text("**Fuck off** My main sex commands are here as follows /start /about /help ")
 
 @VINESH.on_message(filters.command("help"))
 async def help_cmd(client, message):
-    await message.reply_text("```ಹೋಗಿ ಹುಡ್ಗೀರ್ ತುಲ್ಲು ನೆಕ್ಕು```")
+    await message.reply_text("```ಹೋಗಿ ಹುಡ್ಗೀರ್ ತುಲ್ಲು ನೆಕ್ಕು  ಸೂಲೆಮಗನೇ```")
+
+@VINESH.on_message(filters.command("love"))
+async def love_cmd(client, message):
+    await message.reply_text("```ಮಿಡಿಲ್ ಕ್ಲಾಸ್ ಹುಡುಗ 2022 - ಕನ್ನಡ ಡಬ್ #HDRip
+
+250MB
+https://rocklinks.net/Rl4FFOja
+
+400MB
+https://rocklinks.net/shvL
+
+720p 800MB
+https://rocklinks.net/dv7ICGB9
+
+1080p 1.3GB
+https://rocklinks.net/5B7hCoX
+
+✅👇👇 How to download or watch movie 
+https://t.me/mdisk_kannada_movie/11
+
+MXPlayer App ಮೂಲಕ ಯಾವುದೇ ಅಡೆ ತಡೆ ಇಲ್ಲದೆ ವೀಕ್ಷಿಸಿ ಅಥವಾ  ಡೌನಲೋಡ್ ಮಾಡಿ```")
 
     
 print("Bot started")
