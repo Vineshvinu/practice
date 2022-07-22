82e82968103282efd096aaed6e7a4bea3e2d6593
Vineshvinu <92975459+Vineshvinu@users.noreply.github.com>
2022-07-22 19:23:11 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,7 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+public class Main {
+    public static void main(String[] args) {
+        // Initialize Api Context
+        ApiContextInitializer.init();
