715b63ada5007c18a6bde5744e266b7d18b0f8b6
Vineshvinu <92975459+Vineshvinu@users.noreply.github.com>
2022-07-26 21:03:40 +0530

    Update start.py

--- start.py
+++ start.py
@@ -2,3 +2,19 @@ from pyrogram import Client, filters
 from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
 import humanize
 from helper.database import  insert
+
+@Client.on_message(filters.private & filters.command(["start"]))
+async def start(client,message):
+	insert(int(message.chat.id))
+	await message.reply_text(text =f"""
+        𝗛𝗲𝗹𝗹𝗼 {message.from_user.first_name }
+	__Ok fine What is your ambition I mean goal__
+	""",reply_to_message_id = message.message_id ,  
+	reply_markup=InlineKeyboardMarkup([[
+          InlineKeyboardButton("😎" ,url="https://t.me/All_language_movie_request_group"), 
+	  InlineKeyboardButton("😍", url="https://t.me/Tamil_Hackers_Moviess")
+          ],[
+          InlineKeyboardButton("😳", url="https://t.me/Tamil_Hackers_Moviess")
+          ]]
+          )
+        )
